37e381e9ae276ec599a65a15141c39527e524952
EugenuSkok <eugenu0123456789@gmail.com>
2024-12-07 10:05:47 +0600

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 def isPalindrom(word):
-    cleaned = word.lower().replace(" ", "")
-    cleaned1 = word.lower().replace(".", "")
+    cleaned = word.lower().replace(" ", "").replace(",", "")
     return cleaned == cleaned[::-1]
 print(isPalindrom("l,evel"))
+
+___________________________________________
+def isPalindrom(word):
+    cleaned = word.lower().replace(" ", "")
+    return cleaned == cleaned[::-1]
+
+def isPalindromList(words):
+    palindromes = []
+    for word in words:
+        if isPalindrom(word):
+            palindromes.append(word)
+    return palindromes
+print(isPalindromList(["hello", "list", "level"]))  
+
+
